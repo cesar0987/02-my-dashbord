721f72ffecaf52f8d3ea3bf681f7a854f352a173
cesar0987 <cesarbenitez46@gmail.com>
2025-04-28 18:47:45 -0300

    changes

--- src/app/dashboard/counter/page.tsx
+++ src/app/dashboard/counter/page.tsx
@@ -1,8 +1,32 @@
 
+import { useState } from "react";
+
+export const metadata = {
+  title: "Counter Page",
+  description: "Contador Client Side",
+};
+
+
 export default function CounterPage() {
+  
+  const [count, setCount] = useState(5);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+  const decrement = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  };
+
+
   return (
-    <div>
-      <h1>Page Counter</h1>
+    <div className="flex flex-col items-center justify-center h-screen w-full h-full">
+      <span>Productos del Carrito</span>
+      <span className="text-9xl">{count}</span>
+     
+      </div>
     </div>
   );
 }
---
+++ src/app/shopping-cart/components/CartCounter.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const CartCounter = () => {
+  return (
+    <div>CartCounter</div>
+  )
+}
